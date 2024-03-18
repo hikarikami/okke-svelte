9ff8f50b1377f911f58be4a59bc08a3927052f07
Ryan Keon <r.w.keon@gmail.com>
2024-03-18 22:48:16 +1030

    Fixed missing page for initial build

---
+++ src/routes/cashflow/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	// Components
+	import PageHeader from '$lib/app/PageHeader.svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+	import { Switch } from '$lib/components/ui/switch/index.js';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<svelte:head>
+	<title>Invoices</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="container mx-auto">
+	<PageHeader
+		title="Cashflow"
+		buttons={[{ label: 'blah', href: '#', variant: 'default', showIcon: true }]}
+	/>
+</div>
+<div>
+	<h1>Cashflow</h1>
+</div>
