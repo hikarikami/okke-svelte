4acb7d766167470df5178358c7afa57667c1469d
Ryan Keon <r.w.keon@gmail.com>
2024-03-20 23:01:37 +1030

    Added charts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 		<Header></Header>
 		<main class="flex flex-row h-full">
 			<Sidebar />
-			<div class="h-full w-full overflow-auto pt-10">
+			<div class="h-full w-full overflow-auto pt-12 pb-32">
 				<slot />
 			</div>
 		</main>
